6633f017e38d3d61b4650fc2961e16dd3f5bcd64
Matías Olivera <matibjolivera@gmail.com>
2021-06-16 19:11:45 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <Header></Header>
+  </div>
+</template>
+
+<script>
+
+import Header from "./components/Header";
+
+export default {
+  name: 'App',
+  components: {
+    Header
+  }
+}
+</script>
+
+<style>
+body {
+  background: #232323;
+  margin: 0;
+  font-family: "Montserrat", "Avenir", serif;
+}
+
+h1 {
+  font-weight: normal;
+  line-height: 1.1;
+  padding: 20px 0;
+
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,41 @@
+<template lang="html">
+  <div class="header">
+    <h1>The Great <br>
+      <span id="colorDisplay">RGB</span>
+      <br>
+      Guessing Game</h1>
+  </div>
+</template>
+
+<script lang="js">
+
+export default {
+  name: 'src-components-header',
+  props: [],
+  mounted() {
+
+  },
+  data() {
+    return {}
+  },
+  methods: {},
+  computed: {}
+}
+
+
+</script>
+
+<style scoped lang="css">
+.header {
+  transition: all 0.3s;
+  background: steelblue;
+  text-transform: uppercase;
+  text-align: center;
+  margin: 0;
+  color: white;
+}
+
+#colorDisplay {
+  font-size: 200%;
+}
+</style>
